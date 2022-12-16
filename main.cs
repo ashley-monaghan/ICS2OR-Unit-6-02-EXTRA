dd636d4b7c5df070e1fe78e3079cd05546ff9c90
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-16 03:02:03 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program {
+  public static async Task Main()
+    {
+
+      string userAnswer;
+      int counter = 0;
+
+        Console.WriteLine("Hello user, would you like to quit?");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter either yes or no ");
+        Console.WriteLine("");
+        userAnswer = (Console.ReadLine());
+
+        while (userAnswer == "no") 
+        {
+            counter++;
+            string text = "Total: " + counter; 
+          
+            await File.WriteAllTextAsync("WriteText.txt", text);
+            Console.WriteLine("File Updated...");
+            Console.WriteLine("");
+            
+            var someText = await File.ReadAllTextAsync(@"WriteText.txt");
+            Console.WriteLine(someText);
+            Console.WriteLine("");
+            
+            Console.WriteLine("How about now? Enter either yes or no: ");
+            userAnswer = (Console.ReadLine());
+            Console.WriteLine("");
+        };
+
+            Console.WriteLine("Good bye!");
+            Console.WriteLine("\nDone.");
+    }
+}
